189e4d8856c3d8023d521f8b954156bdc28eb4b5
gizemsarsinlar <33731743+gizemsarsinlar@users.noreply.github.com>
2017-11-18 00:10:43 +0300

    Add files via upload

---
+++ Q3.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+	puts("Values 150 and 5:");
+	printf("Addition is %d\n", 150+5);
+	printf("Subraction is %d\n", 150-5);
+	printf("Multiplication is %d\n", 150*5);
+	printf("Division is %d\n", 150/5);
+	return 0;
+}
