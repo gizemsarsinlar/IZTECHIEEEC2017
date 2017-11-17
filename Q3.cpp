1e271d1df2768b9aab33bae24cc1ec1c1ca5f2cc
gizemsarsinlar <33731743+gizemsarsinlar@users.noreply.github.com>
2017-11-18 00:17:26 +0300

    Delete Q3.cpp

--- Q3.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(){
-	puts("Values 150 and 5:");
-	printf("Addition is %d\n", 150+5);
-	printf("Subraction is %d\n", 150-5);
-	printf("Multiplication is %d\n", 150*5);
-	printf("Division is %d\n", 150/5);
-	return 0;
-}
